7538a59481743808ed2cea2589c8ab1eaad59a17
simba <simbakriss@gmail.com>
2024-04-18 00:56:27 +0300

    Add Paste and Tag Models with their fields and migrate to database

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Tag, Paste
 # Register your models here.
+
+class PasteAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Paste, PasteAdmin)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from django.utils.crypto import get_random_string
+from random import randint
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Tags"
 
 
 class Paste(models.Model):
@@ -13,13 +32,37 @@ class Paste(models.Model):
         PRIVATE: "Private"
     }
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
     content = models.TextField()
+    slug = models.SlugField(unique=True, blank=True)
     paste_exposure = models.CharField(
         max_length=10, choices=EXPIRY_OPTIONS, default=UNLISTED
     )
     expiry_date = models.DateTimeField(blank=True, null=True)
-    title = models.CharField(max_length=250, blank=True, null=True)
+    title = models.CharField(max_length=250, blank=True,
+                             null=True, default="Untitled")
     burn_after_reading = models.BooleanField(default=False)
     password = models.BooleanField(default=False)
     password_text = models.CharField(blank=True, null=True, max_length=250)
+    tags = models.ManyToManyField(Tag, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.content}"
+
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+            string_to_slugify = f'{get_random_string(8)} {randint(1, 30)}'
+            self.slug = slugify(string_to_slugify)[:8]
+            
+            i = 0
+            while Paste.objects.filter(slug=self.slug).exists():
+                self.slug = string_to_slugify[:8+i]
+
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Pastes"
